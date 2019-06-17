c6f7f43765e6e3cf6c19314380b383ea5cbc36b7
vmalik <vinayakmalik1999@gmail.com>
2019-06-16 23:25:08 -0500

    minor edits

--- form-data/scripts/main.js
+++ form-data/scripts/main.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 $.getJSON("https://kt-dev.outsystemscloud.com/PWABack/rest/EmployeeList/Get", function(data){
-=======
-$.getJSON("https://kt-dev .outsystemscloud.com/PWABack/rest/EmployeeList/Get", function(data){
->>>>>>> b0787c6d1983ce7dc72b8c58d198b97dfbaf42c6
-console.log(data)
+
 
 });
