d7c8c7f7385d756ba9459fd7bc481f3741401ce3
vmalik <vinayakmalik1999@gmail.com>
2019-06-18 11:28:52 -0500

    passing id from createNewCard to edit details and other changes

--- form-data/scripts/main.js
+++ form-data/scripts/main.js
@@ -5,7 +5,7 @@ document.body.onload = cardsToCreate();
 
 function createNewCard(i) {
 
-$("<div class = column> <div class= card> <div class= container> <h2> "+ data[i].Name + "</h2> <p class= title>"+ data[i].Id +"</p> <p> (+1)" + data[i].PhoneNumber +"</p> <p>"+ data[i].Email+ " </p>  <p><button class= button>Edit Details</button></p> </div>  </div></div>" ).insertAfter(".row")
+$("<div class = column> <div class= card> <div class= container> <h2> "+ data[i].Name + "</h2> <p class= title>"+ data[i].Id +"</p> <p> (+1)" + data[i].PhoneNumber +"</p> <p>"+ data[i].Email+ " </p>  <p><button class = button onClick = editDetails("+ data[i].Id +") >Edit Details</button></p> </div>  </div></div>" ).insertAfter(".row")
 
 }
 //function for number of cards to create
@@ -64,4 +64,8 @@ function nameSeperate(name){
   var newName = {firstName:first,lastName:last}
   return (newName);
 
+}
+//function to edit employee details using POST method
+function editDetails(event){
+    alert(event);
 }
