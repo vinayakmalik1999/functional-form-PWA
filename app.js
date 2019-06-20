d289c3fadd84cd36c3c0c758421b97300318f655
vmalik <vinayakmalik1999@gmail.com>
2019-06-20 17:45:25 -0500

    adding data

--- app.js
+++ app.js
@@ -3,30 +3,39 @@ const app = express()
 const path = require('path');
 const router = express.Router();
 var session = require('client-sessions');
+const axios = require('axios');
+const requestURL = "https://kt-dev.outsystemscloud.com/PWABack/rest/EmployeeDetail/GET";
 
+// setting view engine template to pug
+app.set('view engine', 'pug')
+//function to get ID from URL passed
 
+//using axios to get data from URL
+axios({
+  method: 'get',
+  url: requestURL,
+  headers: {
+    EmployeeId: '1'
+  }})
+.then((res) => {
+  console.log(res.data);
+});
 /*
 * Route to render HTML Page
 */
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/form-data/form.html'));
-  //__dirname : It will resolve to your project folder.
+  //__dirname : It will resolve to  project folder.
 });
+//edit details routing
 app.get('/form/*',function (req,res){
-
-    res.sendFile(path.join(__dirname+'/form-data/form.html'),
-
-  );
+  
+  res.render('index', {title: 'Hey'},);
 
 });
 
-
+// used for giving access to static files
 app.use(express.static('form-data'));
-
+//starting local server
 app.listen(process.env.port || 3000);
 console.log('Running at Port 3000');
-
-
-function foo(index){
-
-}
